7432d94091fceacb744a8712257afa4727794ed0
MamathaNeella <157876261+MamathaNeella@users.noreply.github.com>
2024-02-23 19:42:28 +0530

    Added JS file with operators

---
+++ Prac.js
@@ -0,0 +1,18 @@
+// Java Script 
+let x=10;
+let y=20;
+document.write("x "+" + " +"y "+"= "+(x+y)+("<br>"));
+// Operators in JS
+
+// Arthmetic Operators
+let a=30;
+let b=90;
+let c=45;
+document.write("a "+"+ "+"b "+"= "+(a+b)+("<br>"));
+document.write("a "+"- "+"b "+"= "+(a-b)+("<br>"));
+document.write("a "+"* "+"b "+"= "+(a*b)+("<br>"));
+document.write("a "+"% "+"b "+"= "+(a%b)+("<br>"));
+document.write("a "+"/ "+"b "+"= "+(a/b)+("<br>"));
+// Negate
+let d= 90
+document.write((-d)+("<br>"));
